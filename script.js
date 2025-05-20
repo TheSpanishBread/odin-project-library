e824dbc7dade9039f42f7967312aa7eb5afc2847
TheSpanishBread <61657019+TheSpanishBread@users.noreply.github.com>
2025-05-21 02:49:35 +0800

    Completed

--- script.js
+++ script.js
@@ -1,157 +1,167 @@
-/////html elements
-
-const addBook = document.querySelector("#addBook");
+///get dom elements
+const addBookButton = document.querySelector("#addBook");
 const shelf = document.querySelector("#shelf");
 const body = document.querySelector("body");
+let myLibrary = [];
+///functions
+function overlay(){
+    let addoverlay = document.createElement("div");
+    addoverlay.id = "overlay";
+    addoverlay.classList = "overlay";
+    body.appendChild(addoverlay);
+ }
+function bookForm(){
+    let formContainer = document.createElement("div");
+        formContainer.classList = "formContainer";
+        formContainer.id = "formContainer";
+    let formContainerTitle = document.createElement("h1");
+        formContainerTitle.textContent = "Enter Book Details"
+    let formAuthorLabel = document.createElement("label");
+        formAuthorLabel.htmlFor = "author";
+        formAuthorLabel.textContent = "Author: ";
+    let formAuthorInput = document.createElement("input");
+        formAuthorInput.placeholder = "Enter book author...";
+        formAuthorInput.id = "author";
+        formAuthorInput.required = true;
+    let formTitleLabel = document.createElement("label");
+        formTitleLabel.htmlFor = "title";
+        formTitleLabel.textContent = "Book title:";
+    let formTitleInput = document.createElement("input");
+        formTitleInput.placeholder = "Enter book title";
+        formTitleInput.id = "title";
+        formTitleInput.required = "true"
+    let formPagesLabel = document.createElement("label");
+        formPagesLabel.htmlFor = "pages";
+        formPagesLabel.textContent = "Enter Number of pages:"
+    let formPagesInput = document.createElement("input");
+        formPagesInput.type = "number";
+        formPagesInput.id = "pages";
+        formPagesInput.required = true;
+    let formReadLabel = document.createElement("label");
+        formReadLabel.htmlFor = "read";
+        formReadLabel.textContent = "Have you read this book?   ";
+    let formReadInput = document.createElement("input");
+        formReadInput.type = "checkbox";
+        formReadInput.id = "read";
+    let formSubmitButton = document.createElement("button");
+        formSubmitButton.textContent = "Add book to shelf";
+        formSubmitButton.id = "formSubmitButton";
+    formContainer.appendChild(formContainerTitle);
+    formContainer.appendChild(formAuthorLabel);
+    formContainer.appendChild(formAuthorInput);
+    formContainer.appendChild(formTitleLabel);
+    formContainer.appendChild(formTitleInput);
+    formContainer.appendChild(formPagesLabel);
+    formContainer.appendChild(formPagesInput);
+    formContainer.appendChild(formReadLabel);
+    formReadLabel.appendChild(formReadInput);
+    formContainer.appendChild(formSubmitButton);
+    body.appendChild(formContainer);
 
-
-
-/////events
-addBook.addEventListener("click", function(){
-    
+}
+function removeForm(){
+    let addoverlay = document.querySelector("#overlay");
+    let formContainer = document.querySelector("#formContainer");
+    body.removeChild(addoverlay);
+    body.removeChild(formContainer);
+}
+///event
+addBookButton.addEventListener("click", function(){
     overlay();
     bookForm();
-    let submit = document.querySelector("#submitButton");
-        submit.addEventListener("click", function(){
-            let authorInput = document.querySelector("#author").value;
-            let titleInput = document.querySelector("#title").value;
-            let pageInput = document.querySelector("#pages").value;
-            let readInput = document.querySelector("#read").value ;
-            let bookID = crypto.randomUUID();
-    
-                
+    ///process book
+    let formSubmitButton = document.querySelector("#formSubmitButton");
+    formSubmitButton.addEventListener("click", function (){
+            let author = document.querySelector("#author").value;
+            let title = document.querySelector("#title").value;
+            let page = document.querySelector("#pages").value;
+            let read = document.querySelector("#read");
+            if (read.checked){
+                read.value = true;
+            } else{
+                read.value = false;
+            }
+            let bookID = "book" + crypto.randomUUID();
+            myLibrary.push(bookID);
+                //save object
             function bookDetailsProcess(bookID, author, title, page, read){
                 this.author = author;
                 this.title = title;
                 this.page = page;
-                this.read = read;   
+                this.read = read.value;   
                 this.bookID = bookID;
+               
             }
-
-            bookDetailsProcess.prototype.changeStatus = function(){
-
+            ///book design
+            bookDetailsProcess.prototype.bookDesign = function (){
+                let bookDesign = document.createElement("div"); 
+                bookDesign.id = this.bookID;   
+                bookDesign.style.height = "10rem";
+                bookDesign.style.width = "2rem";
+                bookDesign.style.backgroundColor = " rgb(" + Math.random()*256  + "," + Math.random()*256  + "," + Math.random()*256 + ")";
+                bookDesign.style.border = "0.3rem solid black";
+                shelf.appendChild(bookDesign);
             };
+            ///book hover
+            bookDetailsProcess.prototype.bookHoverDetails = function(){
+                let bookWhole = document.querySelector("#" + this.bookID);
+                bookWhole.addEventListener("mouseover", () => {
 
+                    let hoverDetails = document.createElement("div");
+                        hoverDetails.id = "hoverDetails";
+                    let hoverDetailsAuthor = document.createElement ("p");
+                        hoverDetailsAuthor.textContent  = "Author: " + this.author;
+                    let hoverDetailsTitle = document.createElement("p");
+                        hoverDetailsTitle.textContent = "Title: " + this.title;
+                    let hoverDetailsPages = document.createElement("p");
+                        hoverDetailsPages.textContent = "Pages: "  + this.page;
+                    let hoverDetailsRead = document.createElement("p");
+                        hoverDetailsRead.textContent = "Read: " + this.read;
+                    let hoverDetailsBookID = document.createElement("p");
+                        hoverDetailsBookID.textContent = "Book ID: " + this.bookID;
 
-            let bookDetails = new bookDetailsProcess(bookID, authorInput, titleInput, pageInput, readInput);
-            addEventListener("click", function(){
+                        hoverDetails.appendChild(hoverDetailsAuthor);
+                        hoverDetails.appendChild(hoverDetailsTitle);
+                        hoverDetails.appendChild(hoverDetailsPages);
+                        hoverDetails.appendChild(hoverDetailsRead);
+                        bookWhole.appendChild(hoverDetails);
+                    
 
-            })
+                });
+                //book mouse out remove hover
+                bookWhole.addEventListener("mouseout", () =>{
+                     let hoverDetails = document.querySelector("#hoverDetails");
+                     bookWhole.removeChild(hoverDetails);
+                        });
+            }
+            bookDetailsProcess.prototype.removeBookIcon = function(){
+                    let deleteBookbutton = document.createElement("button");
+                    let deleteIcon = document.createElement("img");
+                    let bookDesign = document.querySelector("#" + this.bookID);
+                    deleteBookbutton.id = "delete-" + this.bookID;
+                    deleteIcon.src = "delete-svgrepo-com.svg";
+                    deleteIcon.style.width = "1.5rem";
 
-        }); 
+                    deleteBookbutton.style.position = "relative";
+                    deleteBookbutton.style.top = "8rem";
+                    deleteBookbutton.appendChild(deleteIcon);
+                    bookDesign.appendChild(deleteBookbutton);  
+            }
+            bookDetailsProcess.prototype.removeBook = function(){
+                let deleteBookbutton = document.querySelector("#delete-" + this.bookID);
+                deleteBookbutton.addEventListener("click", () =>{
+                        let bookDesign = document.querySelector("#" + this.bookID);
+                        shelf.removeChild(bookDesign);
+                        myLibrary = myLibrary.filter(id => id !== this.bookID);
+                });
 
+            }
+             myLibrary[bookID] = new bookDetailsProcess(bookID, author, title, page, read);
+            myLibrary[bookID].bookDesign();
+            myLibrary[bookID].bookHoverDetails();
+            myLibrary[bookID].removeBookIcon();
+            myLibrary[bookID].removeBook();
+            removeForm();
+    });
 
-    //save the form in an object
-
-    //use object constructor
-
-    //use prototype for the read or not read yet
-    
-    /*
-    // add these when the form is done
-    let book = bookDesign();
-    let delBook = removeBook();
-    append(book, delBook); 
-*/
-
-
-
-
-    
 });
-
-////functions////
-function bookForm(){
-    let container = document.createElement("div");
-    container.classList = "container";
-    
-    let authorLabel = document.createElement("label");
-    let author = document.createElement("input");
-    authorLabel.htmlFor = "author";
-    authorLabel.textContent = "Author:"
-
-    author.id = "author";
-    author.placeholder = "Enter book author..."
-    
-    
-    let titleLabel = document.createElement("label");
-    let title = document.createElement("input");
-    titleLabel.htmlFor = "title";
-    titleLabel.textContent = "Title:"
-
-    title.id = "title";
-    title.placeholder = "Enter book title..."
-
-    let numberLabel = document.createElement("label");
-    let number = document.createElement("input");
-
-    numberLabel.htmlFor = "pages";
-    numberLabel.textContent = "Number of pages";
-
-    number.id="pages";
-    number.type = "number";
-
-    let readLabel = document.createElement("label");
-    let read = document.createElement("input");
-
-    readLabel.htmlFor = "read";
-    readLabel.textContent = "Have you read the book?"
-
-    read.type = "checkbox";
-    read.id = "read";
-    read.value = "true";
-
-    let submitButton = document.createElement("button");
-    submitButton.id = "submitButton";
-    submitButton.textContent = "Submit";
-
-
-    container.appendChild(authorLabel);
-    container.appendChild(author);
-    container.appendChild(titleLabel);
-    container.appendChild(title);
-    container.appendChild(numberLabel);
-    container.appendChild(number);
-    container.appendChild(readLabel);
-    container.appendChild(read);
-    container.appendChild(submitButton);
-    body.appendChild(container);
-
-
-}
-
-function overlay(){
-    let addoverlay = document.createElement("div");
-    addoverlay.classList = "overlay";
-    body.appendChild(addoverlay);
-}
-function bookDesign(){
-    let book = document.createElement("div");
-    
-
-    book.style.height = "10rem";
-    book.style.width = "2rem";
-    book.style.backgroundColor = " rgb(" + Math.random()*256  + "," + Math.random()*256  + "," + Math.random()*256 + ")";
-    book.style.border = "0.3rem solid black";
-
-    return book;
-    
-}
-function removeBook(){
-    let deleteBook = document.createElement("button");
-    let deleteIcon = document.createElement("img");
-    
-    deleteIcon.src = "delete-svgrepo-com.svg";
-    deleteIcon.style.width = "1.5rem";
-
-    deleteBook.style.position = "relative";
-    deleteBook.style.top = "8rem";
-    deleteBook.appendChild(deleteIcon);
-    
-    return deleteBook;
-}
-function append(book, deleteBook){
-    
-    book.appendChild(deleteBook);
-    shelf.appendChild(book);
-}
